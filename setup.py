de248bc06e71dd40deb4a4b1ff35dc59a7249775
shashwat-projects <mishrashashwat066@gmail.com>
2025-09-17 11:31:33 +0530

    removed unnecessary folders

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages,setup
 
 setup(
-    name='mcqgenerator',
+    name='GenAIForge',
     version='0.0.1',
     author='shashwat mishra',
     author_email='mishrashashwat066@gmail.com',
-    install_requires=["huggingface-hub","langchain","streamlit","python-dotenv","PyPDF2"],
+    install_requires=["huggingface-hub","langchain","langchain-huggingface", "streamlit","python-dotenv","PyPDF2", "streamlit", "pandas", "os", "warnings", "json", "traceback", "logging"],
     packages=find_packages()
 )
